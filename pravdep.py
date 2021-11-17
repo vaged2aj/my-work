617388fc64693baced1ef1f535f6c62365dc8e05
vaged2aj <dva.vag@gmail.com>
2021-11-17 19:42:54 +0100

    second

---
+++ pravdep.py
@@ -0,0 +1,30 @@
+from random import randint
+from random import seed
+from random import choice
+import random
+import time
+import os
+import math
+from datetime import date
+import pickle
+import recover
+
+
+def abc(num):
+    a = 0
+    b = 0 
+    for i in range(0,num):
+        
+        xi = random.choice([True, False])
+        #print(xi)
+        if xi == True:
+            return True
+         
+def wrap(num,num2):
+    odo = 0
+    aga = num
+    while num > 0:
+        num -= 1
+        if abc(num2) == True:
+           odo += 1 
+    return odo/aga
