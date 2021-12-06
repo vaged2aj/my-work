d060b4ccd916bbfdc0533adc628132ec9595025c
Dali <83604150+vaged2aj@users.noreply.github.com>
2021-12-06 10:22:06 +0100

    Update kruh.py

--- kruh.py
+++ kruh.py
@@ -1,82 +1,8 @@
-####x = 3  #Create variable x and assign value 3 to it
-####x = x*x  #Bind x to value 9
-####print x
-####y = raw_input('enter a number:')
-####print type(y)
-####print y
-####y = float(raw_input('Enter a number: '))
-####print type(y)
-####print y
-####print y*y
-####
-####x = int(raw_input('Enter an integer: '))
-####if x%2 == 0:
-####    print 'Even'
-####else:
-####    print 'Odd'
-####    if x%3 != 0:
-####        print 'And not divisible by 3'
-####
-####x = int(raw_input('Enter x: '))
-####y = int(raw_input('Enter y: '))
-####z = int(raw_input('Enter z: '))
-####
-####if x < y:
-####    if x < z:
-####        print 'x is least'
-####    else:
-####        print 'z is least'
-####else:
-####    print 'y is least'
-####
-####if x < y:
-####    if x < z:
-####        print 'x is least'
-####    else:
-####        print 'z is least'
-####elif y < z:
-####    print 'y is least'
-####else:
-####    print 'z is least'  
-####
-####if x < y and x < z:
-####    print 'x is least'
-####elif y < z:
-####    print 'y is least'
-####else:
-####    print 'z is least'
-####    
-####
-#####Find the cube root of a perfect cube
-###x = int(raw_input('Enter an integer: '))
-###y = int(raw_input('Enter an integer: '))
-###ans = 0
+# počítá pí házením šipek do kruhu
+
 import random
 import math
-###x = math.pi
-###y = 0
-###z = int(0)
-###x = random()
-###print x
-###print y
-###print y
-###while ans*ans*ans < abs(x):
-###    ans = ans + 1
-###    print 'current guess =', ans
-### je v jednotkovem ctverci?
-###if x < 1:
-###    if y < 1:
-###        print 'je v jednotkovem ctverci'
-###else:
-###    print 'neni v jednotkovem ctverci'
-##var = 8 
-##while (var < 200000):
-##    alpha = float(math.pi / var) 
-##    beta = 2 * (math.sin(alpha))
-##    obvod = beta * var
-##    pi = obvod / 2
-##    var = var * 2 
-##print pi
+
 def pi(darts):
    count = int(0)
    z = 0
@@ -117,13 +43,6 @@ def run2(rounds,darts):
       z += pi(darts)
       rounds -= 1
    return z/a
-      
-
-      
-###
-##
-
-
 
 def simple(x):
    def last(z):      
